6dc93e00da3297e5d4bf209ceec7328cf9d05004
ElectroMaster05 <al060878@uacam.mx>
2021-11-10 20:57:26 -0600

    se agregan los archivos

---
+++ test.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static junit.framework.Assert.assertEquals;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author maast
+ */
+public class test {
+    
+     
+   //test1 Positive = new test1();
+   //Palindromo p1= new Palindromo(); 
+   idMaestroAlumno b= new idMaestroAlumno();
+   
+   /*
+    @Test
+    public void testSomething() {
+        assertEquals(15, Positive.sum(new int[]{1,2,3,4,5}));
+        assertEquals(13, Positive.sum(new int[]{1,-2,3,4,5}));
+        assertEquals(0, Positive.sum(new int[]{}));
+        assertEquals(0, Positive.sum(new int[]{-1,-2,-3,-4,-5}));
+        assertEquals(9, Positive.sum(new int[]{-1,2,3,4,-5}));
+        
+        
+        
+    } 
+      
+      @Test
+    public void testPalindrome() {
+        assertEquals(true, p1.checkpalindrome("oso"));
+       assertEquals(true, p1.checkpalindrome("ana"));
+       assertEquals(true, p1.checkpalindrome("holi"));
+    }   
+    
+    */
+    
+    public test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+ 
+        @Test
+    public void testID() {
+        assertEquals("Alumno_OK", b.checkID(2,"al608780"));
+       assertEquals("Alumno_OK", b.checkID(2,"al011120"));
+       assertEquals("Maestro_OK", b.checkID(1,"1001"));
+       assertEquals("Maestro_OK", b.checkID(1,"1002"));
+    }  
+
+    
+          @Test
+    public void testID2() {
+        assertEquals("ERROR01", idMaestroAlumno.checkID(1,"999"));
+       assertEquals("ERROR01", idMaestroAlumno.checkID(1,"10001"));
+       assertEquals("ERROR02", idMaestroAlumno.checkID(2,"al999"));
+       assertEquals("ERROR02", idMaestroAlumno.checkID(2,"al1671119"));
+    }  
+
+ 
+           @Test
+    public void testID3() {
+        assertEquals("Intendencia_OK", b.checkID(3,"in000"));
+       assertEquals("Intendencia_OK", b.checkID(3,"in123"));
+       assertEquals("ERROR03", b.checkID(3,"1001"));
+       assertEquals("ERROR03", b.checkID(3,"in1002"));
+    }  
+
+    
+    
+}
